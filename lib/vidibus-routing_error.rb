fa98f4b71fa2b086a6395566000630fb623c54e4
Christopher Sexton <csexton@gmail.com>
2012-03-19 13:36:59 -0400

    Prevent loading at all on test and cucumber

--- lib/vidibus-routing_error.rb
+++ lib/vidibus-routing_error.rb
@@ -1,8 +1,8 @@
-require 'vidibus/routing_error/rack'
+unless Rails.env.test? || Rails.env.cucumber?
+  require 'vidibus/routing_error/rack'
 
-module Vidibus::RoutingError
-  class Engine < ::Rails::Engine
-    unless Rails.env.test? || Rails.env.cucumber?
+  module Vidibus::RoutingError
+    class Engine < ::Rails::Engine
       config.app_middleware.insert_after('ActionDispatch::ShowExceptions', 'Vidibus::RoutingError::Rack')
     end
   end
