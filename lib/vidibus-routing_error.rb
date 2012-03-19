458e5c822031f2491af1d057e4f0e6bd25581e4c
Christopher Sexton <csexton@gmail.com>
2012-03-19 11:57:44 -0400

    Check for cucumber enviroment.

--- lib/vidibus-routing_error.rb
+++ lib/vidibus-routing_error.rb
@@ -2,7 +2,7 @@ require 'vidibus/routing_error/rack'
 
 module Vidibus::RoutingError
   class Engine < ::Rails::Engine
-    unless Rails.env.test?
+    unless Rails.env.test? || Rails.env.cucumber?
       config.app_middleware.insert_after('ActionDispatch::ShowExceptions', 'Vidibus::RoutingError::Rack')
     end
   end
